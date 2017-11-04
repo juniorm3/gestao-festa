21e1e01e661919155ce7023ca51044722d6b400f
Mario Junior <juniorm3@msn.com>
2017-11-04 17:59:28 -0200

    Salvando dados em banco

--- src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -3,22 +3,33 @@ package com.algaworks.festa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.festa.model.Convidado;
 import com.algaworks.festa.repository.Convidados;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 	
 	@Autowired
 	private Convidados convidados;
 
-	@GetMapping("/convidados")
+	@GetMapping
 	public ModelAndView listar() {
-		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		ModelAndView mv = new ModelAndView("ListaConvidados");
 		
-		modelAndView.addObject("convidados", convidados.findAll());
+		mv.addObject("convidados", convidados.findAll());
+		mv.addObject(new Convidado());
 		
-		return modelAndView;
+		return mv;
+	}
+	
+	@PostMapping
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
 	}
 }
